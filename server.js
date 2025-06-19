ab22a0508be5e338e3aa9ce166f92d21d7e2cb7c
SashmithaRamesh <sashmitharamesh16@gmail.com>
2025-06-19 20:18:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://sashmi16:Sashmi2005@movieticket.s0pkxve.mongodb.net/?retryWrites=true&w=majority&appName=MovieTicket', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch(err => console.error('MongoDB connection failed:', err));
